d3068c9fb653dd2b2bacf5fa9658468d54c36940
VISTALL <vistall.valeriy@gmail.com>
2014-02-10 23:18:18 +0200

    merge two roots

---
+++ src/org/mustbe/consulo/mono/dotnet/module/extension/MonoDotNetMutableModuleExtension.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013 must-be.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mustbe.consulo.mono.dotnet.module.extension;
+
+import javax.swing.JComponent;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.mustbe.consulo.dotnet.module.extension.DotNetMutableModuleExtension;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.ModifiableRootModel;
+
+/**
+ * @author VISTALL
+ * @since 20.11.13.
+ */
+public class MonoDotNetMutableModuleExtension extends MonoDotNetModuleExtension implements DotNetMutableModuleExtension<MonoDotNetModuleExtension>
+{
+	public MonoDotNetMutableModuleExtension(@NotNull String id, @NotNull Module module)
+	{
+		super(id, module);
+	}
+
+	@Nullable
+	@Override
+	public JComponent createConfigurablePanel(@NotNull ModifiableRootModel modifiableRootModel, @Nullable Runnable runnable)
+	{
+		return createConfigurablePanelImpl(modifiableRootModel, runnable);
+	}
+
+	@Override
+	public boolean isModified(@NotNull MonoDotNetModuleExtension extension)
+	{
+		return isModifiedImpl(extension);
+	}
+}
