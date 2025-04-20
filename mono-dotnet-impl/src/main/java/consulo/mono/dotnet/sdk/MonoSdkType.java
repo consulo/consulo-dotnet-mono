3332466e790a514096f016bd8490ec481e573106
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-20 16:01:08 +0400

    remove usage EP_NAME

--- mono-dotnet-impl/src/main/java/consulo/mono/dotnet/sdk/MonoSdkType.java
+++ mono-dotnet-impl/src/main/java/consulo/mono/dotnet/sdk/MonoSdkType.java
@@ -17,6 +17,7 @@
 package consulo.mono.dotnet.sdk;
 
 import consulo.annotation.component.ExtensionImpl;
+import consulo.application.Application;
 import consulo.application.util.SystemInfo;
 import consulo.content.bundle.*;
 import consulo.dataContext.DataContext;
@@ -37,9 +38,9 @@ import consulo.ui.image.Image;
 import consulo.util.lang.Pair;
 import consulo.virtualFileSystem.LocalFileSystem;
 import consulo.virtualFileSystem.VirtualFile;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import javax.swing.*;
 import java.io.File;
 import java.util.ArrayList;
@@ -53,256 +54,213 @@ import java.util.function.Consumer;
  * @since 06.12.13.
  */
 @ExtensionImpl
-public class MonoSdkType extends DotNetSdkType implements SdkTypeWithCustomCreateUI
-{
-	public static final String ourDefaultLinuxCompilerPath = "/usr/bin/mcs";
-	public static final String ourDefaultFreeBSDCompilerPath = "/usr/local/bin/mcs";
+public class MonoSdkType extends DotNetSdkType implements SdkTypeWithCustomCreateUI {
+    public static final String ourDefaultLinuxCompilerPath = "/usr/bin/mcs";
+    public static final String ourDefaultFreeBSDCompilerPath = "/usr/local/bin/mcs";
 
-	private static final String apiVer = "-api";
+    private static final String apiVer = "-api";
 
-	private static final String[] ourMonoPaths = new String[]{
-			"C:/Program Files/Mono/",
-			"C:/Program Files (x86)/Mono/"
-	};
+    private static final String[] ourMonoPaths = new String[]{
+        "C:/Program Files/Mono/",
+        "C:/Program Files (x86)/Mono/"
+    };
 
-	@Nonnull
-	public String getExecutable(@Nonnull Sdk sdk)
-	{
-		String runFile;
-		if(SystemInfo.isWindows)
-		{
-			runFile = sdk.getHomePath() + "/../../../bin/mono.exe";
-		}
-		else if(SystemInfo.isMac)
-		{
-			runFile = sdk.getHomePath() + "/../../../bin/mono";
-		}
-		else if(SystemInfo.isFreeBSD)
-		{
-			runFile = "/usr/local/bin/mono";
-		}
-		else if(SystemInfo.isLinux)
-		{
-			runFile = "/usr/bin/mono";
-		}
-		else
-		{
-			throw new UnsupportedOperationException(SystemInfo.OS_NAME);
-		}
-		return runFile;
-	}
+    @Nonnull
+    public String getExecutable(@Nonnull Sdk sdk) {
+        String runFile;
+        if (SystemInfo.isWindows) {
+            runFile = sdk.getHomePath() + "/../../../bin/mono.exe";
+        }
+        else if (SystemInfo.isMac) {
+            runFile = sdk.getHomePath() + "/../../../bin/mono";
+        }
+        else if (SystemInfo.isFreeBSD) {
+            runFile = "/usr/local/bin/mono";
+        }
+        else if (SystemInfo.isLinux) {
+            runFile = "/usr/bin/mono";
+        }
+        else {
+            throw new UnsupportedOperationException(SystemInfo.OS_NAME);
+        }
+        return runFile;
+    }
 
-	@Nonnull
-	public static MonoSdkType getInstance()
-	{
-		return EP_NAME.findExtensionOrFail(MonoSdkType.class);
-	}
+    @Nonnull
+    public static MonoSdkType getInstance() {
+        return Application.get().getExtensionPoint(SdkType.class).findExtensionOrFail(MonoSdkType.class);
+    }
 
-	public MonoSdkType()
-	{
-		super("MONO_DOTNET_SDK");
-	}
+    public MonoSdkType() {
+        super("MONO_DOTNET_SDK");
+    }
 
-	@Nonnull
-	@Override
-	public Collection<String> suggestHomePaths()
-	{
-		String defaultHomePath = getDefaultHomePath();
-		if(defaultHomePath == null)
-		{
-			return Collections.emptyList();
-		}
-		File dir = new File(defaultHomePath, "lib/mono");
-		if(!dir.exists())
-		{
-			return Collections.emptyList();
-		}
-		List<String> list = new ArrayList<>(1);
-		for(File file : dir.listFiles())
-		{
-			list.add(file.getPath());
-		}
-		return list;
-	}
+    @Nonnull
+    @Override
+    public Collection<String> suggestHomePaths() {
+        String defaultHomePath = getDefaultHomePath();
+        if (defaultHomePath == null) {
+            return Collections.emptyList();
+        }
+        File dir = new File(defaultHomePath, "lib/mono");
+        if (!dir.exists()) {
+            return Collections.emptyList();
+        }
+        List<String> list = new ArrayList<>(1);
+        for (File file : dir.listFiles()) {
+            list.add(file.getPath());
+        }
+        return list;
+    }
 
-	@Override
-	public boolean canCreatePredefinedSdks()
-	{
-		return true;
-	}
+    @Override
+    public boolean canCreatePredefinedSdks() {
+        return true;
+    }
 
-	@Nullable
-	private String getDefaultHomePath()
-	{
-		if(SystemInfo.isWindows)
-		{
-			for(String monoPath : ourMonoPaths)
-			{
-				File file = new File(monoPath);
-				if(file.exists())
-				{
-					return monoPath;
-				}
-			}
-			return ourMonoPaths[0];
-		}
+    @Nullable
+    private String getDefaultHomePath() {
+        if (SystemInfo.isWindows) {
+            for (String monoPath : ourMonoPaths) {
+                File file = new File(monoPath);
+                if (file.exists()) {
+                    return monoPath;
+                }
+            }
+            return ourMonoPaths[0];
+        }
 
-		if(SystemInfo.isMac)
-		{
-			return "/Library/Frameworks/Mono.framework/Home/";
-		}
+        if (SystemInfo.isMac) {
+            return "/Library/Frameworks/Mono.framework/Home/";
+        }
 
-		if(SystemInfo.isFreeBSD)
-		{
-			File file = new File(ourDefaultFreeBSDCompilerPath);
-			if(file.exists())
-			{
-				return "/usr/local/";
-			}
-		}
+        if (SystemInfo.isFreeBSD) {
+            File file = new File(ourDefaultFreeBSDCompilerPath);
+            if (file.exists()) {
+                return "/usr/local/";
+            }
+        }
 
-		if(SystemInfo.isLinux)
-		{
-			File file = new File(ourDefaultLinuxCompilerPath);
-			if(file.exists())
-			{
-				return "/usr/";
-			}
-		}
-		return null;
-	}
+        if (SystemInfo.isLinux) {
+            File file = new File(ourDefaultLinuxCompilerPath);
+            if (file.exists()) {
+                return "/usr/";
+            }
+        }
+        return null;
+    }
 
-	@Override
-	public boolean isValidSdkHome(String s)
-	{
-		return new File(s, "mscorlib.dll").exists();
-	}
+    @Override
+    public boolean isValidSdkHome(String s) {
+        return new File(s, "mscorlib.dll").exists();
+    }
 
-	@Nullable
-	@Override
-	public String getVersionString(String path)
-	{
-		String directoryName = new File(path).getName();
-		if(directoryName.endsWith(apiVer))
-		{
-			return directoryName.substring(0, directoryName.length() - apiVer.length());
-		}
-		return directoryName;
-	}
+    @Nullable
+    @Override
+    public String getVersionString(String path) {
+        String directoryName = new File(path).getName();
+        if (directoryName.endsWith(apiVer)) {
+            return directoryName.substring(0, directoryName.length() - apiVer.length());
+        }
+        return directoryName;
+    }
 
-	@Override
-	public String suggestSdkName(String currentSdkName, String sdkHome)
-	{
-		File file = new File(sdkHome);
-		return getPresentableName() + " " + file.getName();
-	}
+    @Override
+    public String suggestSdkName(String currentSdkName, String sdkHome) {
+        File file = new File(sdkHome);
+        return getPresentableName() + " " + file.getName();
+    }
 
-	@Nonnull
-	@Override
-	public String getPresentableName()
-	{
-		return "Mono";
-	}
+    @Nonnull
+    @Override
+    public String getPresentableName() {
+        return "Mono";
+    }
 
-	@Nullable
-	@Override
-	public Image getIcon()
-	{
-		return MonoDotNetIconGroup.mono();
-	}
+    @Nonnull
+    @Override
+    public Image getIcon() {
+        return MonoDotNetIconGroup.mono();
+    }
 
-	@Override
-	public void showCustomCreateUI(SdkModel sdkModel, JComponent parentComponent, final Consumer<Sdk> sdkCreatedCallback)
-	{
-		File monoLib = null;
-		if(SystemInfo.isFreeBSD)
-		{
-			File file = new File(ourDefaultFreeBSDCompilerPath);
-			if(!file.exists())
-			{
-				Messages.showErrorDialog(parentComponent, "\'" + ourDefaultFreeBSDCompilerPath + "\' not found. OS: " + SystemInfo.OS_NAME);
-				return;
-			}
-			monoLib = new File("/usr/local/lib/mono");
-		}
-		else if(SystemInfo.isLinux)
-		{
-			File file = new File(ourDefaultLinuxCompilerPath);
-			if(!file.exists())
-			{
-				Messages.showErrorDialog(parentComponent, "\'" + ourDefaultLinuxCompilerPath + "\' not found. OS: " + SystemInfo.OS_NAME);
-				return;
-			}
-			monoLib = new File("/usr/lib/mono");
-		}
-		else if(SystemInfo.isWindows || SystemInfo.isMac)
-		{
-			FileChooserDescriptor singleFolderDescriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor();
-			String toSelectPath = getDefaultHomePath();
-			VirtualFile toSelect = toSelectPath == null ? null : LocalFileSystem.getInstance().findFileByPath(toSelectPath);
-			VirtualFile monoDir = IdeaFileChooser.chooseFile(singleFolderDescriptor, null, toSelect);
-			if(monoDir == null)
-			{
-				return;
-			}
+    @Override
+    @RequiredUIAccess
+    public void showCustomCreateUI(SdkModel sdkModel, JComponent parentComponent, final Consumer<Sdk> sdkCreatedCallback) {
+        File monoLib = null;
+        if (SystemInfo.isFreeBSD) {
+            File file = new File(ourDefaultFreeBSDCompilerPath);
+            if (!file.exists()) {
+                Messages.showErrorDialog(parentComponent, "\'" + ourDefaultFreeBSDCompilerPath + "\' not found. OS: " + SystemInfo.OS_NAME);
+                return;
+            }
+            monoLib = new File("/usr/local/lib/mono");
+        }
+        else if (SystemInfo.isLinux) {
+            File file = new File(ourDefaultLinuxCompilerPath);
+            if (!file.exists()) {
+                Messages.showErrorDialog(parentComponent, "\'" + ourDefaultLinuxCompilerPath + "\' not found. OS: " + SystemInfo.OS_NAME);
+                return;
+            }
+            monoLib = new File("/usr/lib/mono");
+        }
+        else if (SystemInfo.isWindows || SystemInfo.isMac) {
+            FileChooserDescriptor singleFolderDescriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor();
+            String toSelectPath = getDefaultHomePath();
+            VirtualFile toSelect = toSelectPath == null ? null : LocalFileSystem.getInstance().findFileByPath(toSelectPath);
+            VirtualFile monoDir = IdeaFileChooser.chooseFile(singleFolderDescriptor, null, toSelect);
+            if (monoDir == null) {
+                return;
+            }
 
-			monoLib = new File(monoDir.getPath(), "lib/mono");
-		}
+            monoLib = new File(monoDir.getPath(), "lib/mono");
+        }
 
-		if(monoLib == null)
-		{
-			Messages.showErrorDialog(parentComponent, "Current OS is not supported: " + SystemInfo.OS_NAME);
-			return;
-		}
+        if (monoLib == null) {
+            Messages.showErrorDialog(parentComponent, "Current OS is not supported: " + SystemInfo.OS_NAME);
+            return;
+        }
 
-		if(!monoLib.exists())
-		{
-			Messages.showErrorDialog(parentComponent, "File: " + monoLib.getAbsolutePath() + " is not exists.");
-			return;
-		}
+        if (!monoLib.exists()) {
+            Messages.showErrorDialog(parentComponent, "File: " + monoLib.getAbsolutePath() + " is not exists.");
+            return;
+        }
 
-		List<Pair<String, File>> list = new ArrayList<Pair<String, File>>();
+        List<Pair<String, File>> list = new ArrayList<Pair<String, File>>();
 
-		File[] files = monoLib.listFiles();
-		if(files != null)
-		{
-			for(File file : files)
-			{
-				if(isValidSdkHome(file.getAbsolutePath()))
-				{
-					list.add(Pair.create(file.getName(), file));
-				}
-			}
-		}
+        File[] files = monoLib.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (isValidSdkHome(file.getAbsolutePath())) {
+                    list.add(Pair.create(file.getName(), file));
+                }
+            }
+        }
 
-		ActionGroup.Builder b = ActionGroup.newImmutableBuilder();
-		for(final Pair<String, File> pair : list)
-		{
-			b.add(new AnAction(pair.getFirst())
-			{
-				@RequiredUIAccess
-				@Override
-				public void actionPerformed(@Nonnull AnActionEvent anActionEvent)
-				{
-					File path = pair.getSecond();
-					String absolutePath = path.getAbsolutePath();
+        ActionGroup.Builder b = ActionGroup.newImmutableBuilder();
+        for (final Pair<String, File> pair : list) {
+            b.add(new AnAction(pair.getFirst()) {
+                @RequiredUIAccess
+                @Override
+                public void actionPerformed(@Nonnull AnActionEvent anActionEvent) {
+                    File path = pair.getSecond();
+                    String absolutePath = path.getAbsolutePath();
 
-					String uniqueSdkName = SdkUtil.createUniqueSdkName(MonoSdkType.this, absolutePath, SdkTable.getInstance().getAllSdks());
-					Sdk sdk = SdkTable.getInstance().createSdk(uniqueSdkName, MonoSdkType.this);
-					SdkModificator modificator = sdk.getSdkModificator();
-					modificator.setVersionString(getVersionString(absolutePath));
-					modificator.setHomePath(absolutePath);
-					modificator.commitChanges();
+                    String uniqueSdkName = SdkUtil.createUniqueSdkName(MonoSdkType.this, absolutePath, SdkTable.getInstance().getAllSdks());
+                    Sdk sdk = SdkTable.getInstance().createSdk(uniqueSdkName, MonoSdkType.this);
+                    SdkModificator modificator = sdk.getSdkModificator();
+                    modificator.setVersionString(getVersionString(absolutePath));
+                    modificator.setHomePath(absolutePath);
+                    modificator.commitChanges();
 
-					sdkCreatedCallback.accept(sdk);
-				}
-			});
-		}
+                    sdkCreatedCallback.accept(sdk);
+                }
+            });
+        }
 
-		DataContext dataContext = DataManager.getInstance().getDataContext(parentComponent);
+        DataContext dataContext = DataManager.getInstance().getDataContext(parentComponent);
 
-		ListPopup choose = JBPopupFactory.getInstance().createActionGroupPopup("Choose", b.build(), dataContext, JBPopupFactory.ActionSelectionAid.SPEEDSEARCH, false);
+        ListPopup choose = JBPopupFactory.getInstance().createActionGroupPopup("Choose", b.build(), dataContext, JBPopupFactory.ActionSelectionAid.SPEEDSEARCH, false);
 
-		choose.showInCenterOf(parentComponent);
-	}
+        choose.showInCenterOf(parentComponent);
+    }
 }
